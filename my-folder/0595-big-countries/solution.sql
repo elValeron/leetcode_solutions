98a5a40db20fe1f6fc9cb92de18124f68494946c
elvaleron <vbalashov89@gmail.com>
2025-06-28 21:09:05 +0000

    [LeetCode Sync] Runtime - 281 ms (96.25%), Memory - 0.0B (100.00%)

--- my-folder/0595-big-countries/solution.sql
+++ my-folder/0595-big-countries/solution.sql
@@ -1,4 +1,11 @@
 -- Write your PostgreSQL query statement below
-SELECT name, population, area
-FROM world
-WHERE population >= 25000000 or area >= 3000000
+WITH vars as (
+    SELECT 25000000 as pop, 3000000 as ar
+)
+SELECT w.name, w.population, w.area
+  FROM world as w, vars as v
+ WHERE population >= v.pop
+    OR area >= v.ar
+ ORDER BY area DESC;
+
+
