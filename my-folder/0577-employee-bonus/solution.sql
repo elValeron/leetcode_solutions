cada4abba4e47b0b06fa16357b1447a69333c6a8
elvaleron <vbalashov89@gmail.com>
2025-06-29 20:31:34 +0000

    [LeetCode Sync] Runtime - 301 ms (68.26%), Memory - 0.0B (100.00%)

--- my-folder/0577-employee-bonus/solution.sql
+++ my-folder/0577-employee-bonus/solution.sql
@@ -1,7 +1,8 @@
 -- Write your PostgreSQL query statement below
-SELECT e.name, b.bonus --Взять столбец имя из таблицы employee и bonus из таблицы бонус
-FROM employee e -- запрос к таблице employee
-LEFT JOIN bonus b -- присоединение таблицы bonus
-ON e.empId = b.empId -- на основании фильтра если empId есть в двух таблицах
-WHERE b.bonus < 1000 -- после присоединения, фильтруем бонус при условии, что бонус меньше 1000
-OR b.bonus is NULL -- или бонуса нет, и выводим результат. 
+SELECT e.name, b.bonus
+  FROM employee AS e
+  LEFT JOIN bonus AS b
+    ON e.empId = b.empId
+ WHERE b.bonus < 1000
+    OR b.bonus IS NULL
+
